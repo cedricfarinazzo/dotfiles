9f0fad3f217fc1f47e6cf250e7522c11c10a9981
FARINAZZO Cédric <cedric.farinazzo@epita.fr>
2019-12-11 11:53:17 +0100

    i3status: reformat wrapper

--- i3status/.i3status/wrapper.py
+++ i3status/.i3status/wrapper.py
@@ -24,10 +24,11 @@
 # 2, as published by Sam Hocevar. See http://sam.zoy.org/wtfpl/COPYING for more
 # details.
 
-import sys, os, pickle
+import sys
+import os
+import pickle
 import subprocess
 import json
-
 
 
 def gen_color(step=1):
@@ -44,84 +45,87 @@ def gen_color(step=1):
     return color
 
 
+# CONFIG
 
-###### CONFIG 
 
-WHT="#FFFFFF"
-BLK="#000000"
-RED="#FF0011"
-ORG="#FF6100"
-YLW="#E3FF00"
-GRN="#00FF46"
-#COLOR=[GRN, WHT, YLW, ORG, RED]
-STEP=5
-COLOR=[]
+WHT = "#FFFFFF"
+BLK = "#000000"
+RED = "#FF0011"
+ORG = "#FF6100"
+YLW = "#E3FF00"
+GRN = "#00FF46"
+STEP = 5
+COLOR = []
 try:
     with open("/tmp/.i3status_color.dat", "rb") as file:
-        COLOR=pickle.load(file)
-except:
-    COLOR=gen_color(5)
+        COLOR = pickle.load(file)
+except Exception:
+    COLOR = gen_color(5)
     with open("/tmp/.i3status_color.dat", "wb") as file:
         pickle.dump(COLOR, file)
-NBCOLOR=len(COLOR)
+NBCOLOR = len(COLOR)
 
 
-MIN_CPU=-5
-MAX_CPU=106
+MIN_CPU = -5
+MAX_CPU = 106
 
-MIN_RAM=-2
-MAX_RAM=106
+MIN_RAM = -2
+MAX_RAM = 106
 
-MIN_TEMP=19
-MAX_TEMP=80
+MIN_TEMP = 19
+MAX_TEMP = 80
 
-MIN_LOAD=-1.5
-MAX_LOAD=10
+MIN_LOAD = -1.5
+MAX_LOAD = 10
 
-MAX_LEN_WIRELESS_NAME=7
+MAX_LEN_WIRELESS_NAME = 7
 
-## FUNCTION
+# FUNCTION
+
 
 def get_governor():
     """ Get the current governor for cpu0, assuming all CPUs use the same. """
     with open('/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor') as fp:
         return fp.readlines()[0].strip()
 
+
 def get_wireless_json_part(json):
     for e in json:
         try:
             if e['name'] == 'wireless':
                 return e
-        except:
+        except Exception:
             pass
     return None
+
 
 def minimize_wireless_msg(json):
     try:
         text = json['full_text']
-        
+
         if "  no wlan  " in text or "  no wlan  " == text:
             return
 
         pre = text[:text.index('at ')+3]
         mid = text[text.index('at ')+3:text.index(', ')]
         end = text[text.index(', '):]
-        
+
         if len(mid) > MAX_LEN_WIRELESS_NAME:
             mid = mid[:MAX_LEN_WIRELESS_NAME] + '..'
 
         json['full_text'] = pre + mid + end
-    except:
+    except Exception:
         pass
+
 
 def update_cpu(cpujson):
     text = cpujson['full_text']
-    value = text[text.index('u ')+1 : text.index('%')].split(' ')
+    value = text[text.index('u ')+1:text.index('%')].split(' ')
     value = int(''.join(value))
-   
+
     try:
         cpujson.pop('color')
-    except:
+    except Exception:
         pass
 
     color = COLOR[0]
@@ -133,16 +137,17 @@ def update_cpu(cpujson):
             break
     cpujson['color'] = color
 
+
 def update_load(loadjson):
     text = loadjson['full_text']
     text = text.replace('load ', '')
     text = ''.join(text.split(' '))
     text = text.replace(',', '.')
     value = float(text)
-    
+
     try:
         loadjson.pop('color')
-    except:
+    except Exception:
         pass
 
     color = COLOR[0]
@@ -154,6 +159,7 @@ def update_load(loadjson):
             break
     loadjson['color'] = color
 
+
 def get_temp():
     p = subprocess.Popen("sensors", stdout=subprocess.PIPE, shell=False)
     (output, err) = p.communicate()
@@ -161,14 +167,15 @@ def get_temp():
     if p_status != 0:
         return "ERROR", BLK
     output = output.decode("utf-8")
-    
+
     try:
-        cpu_temp = output[output.index("Package id 0: "):output.index("(high = +100.0")]
+        cpu_temp = output[output.index("Package id 0: "):
+                          output.index("(high = +100.0")]
         cpu_temp = cpu_temp.split(':')[1]
         cpu_temp = cpu_temp.split(' ')
         cpu_temp = ''.join(cpu_temp)
-        
-        temp=float(cpu_temp[1:cpu_temp.index("°C")])
+
+        temp = float(cpu_temp[1:cpu_temp.index("°C")])
 
         color = COLOR[0]
         step = (MAX_TEMP - MIN_TEMP) / NBCOLOR
@@ -178,9 +185,10 @@ def get_temp():
                 color = COLOR[i]
                 break
         return cpu_temp, color
-    except:
+    except Exception:
         return "ERROR", BLK
-    
+
+
 def normalize_ram(ram):
     if (ram <= 999):
         return str(ram) + "Mib"
@@ -188,10 +196,13 @@ def normalize_ram(ram):
         ram = round(ram/1000, 1)
         return str(ram) + "Gib"
 
+
 def get_ram_usage():
-    tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
+    tot_m, used_m, free_m = \
+        map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
     usage = int(((used_m/tot_m)*100))
-    ram = " " + str(usage) + "% (" + normalize_ram(used_m) + " / " + normalize_ram(tot_m) +  ") "
+    ram = " " + str(usage) + "% (" + \
+          normalize_ram(used_m) + " / " + normalize_ram(tot_m) + ") "
     color = COLOR[0]
     step = (MAX_RAM - MIN_RAM) / NBCOLOR
     for i in range(NBCOLOR):
@@ -202,13 +213,13 @@ def get_ram_usage():
     return ram, color
 
 
-## WRAPPER
-
+# WRAPPER
 
 def print_line(message):
     """ Non-buffered printing to stdout. """
     sys.stdout.write(message + '\n')
     sys.stdout.flush()
+
 
 def read_line():
     """ Interrupted respecting reader for stdin. """
@@ -222,6 +233,7 @@ def read_line():
     # exit on ctrl-c
     except KeyboardInterrupt:
         sys.exit()
+
 
 if __name__ == '__main__':
     # Skip the first line which contains the version header.
@@ -239,29 +251,36 @@ if __name__ == '__main__':
         j = json.loads(line)
         # insert information into the start of the json, but could be anywhere
         # CHANGE THIS LINE TO INSERT SOMETHING ELSE
-        
-        # j.insert(0, {'full_text' : '%s' % get_governor(), 'name' : 'gov'})
-        
-        
+
         # RAM
         (ram, r_color) = get_ram_usage()
-        j.insert(1, {'name' : 'ram', 'markup' : 'none', 'color' : r_color, 'full_text' : '%s' %ram})
-        
+        j.insert(1,
+                 {
+                     'name': 'ram',
+                     'markup': 'none',
+                     'color': r_color,
+                     'full_text': '%s' % ram
+                 })
 
-        #TEMP
+        # TEMP
         (temp, t_color) = get_temp()
-        j.insert(8, {'name' : 'temperatur', 'markup' : 'none', 'color' : t_color, 'full_text' : '%s' % temp})
-        
-        
-        #CPU
+        j.insert(8,
+                 {
+                     'name': 'temperatur',
+                     'markup': 'none',
+                     'color': t_color,
+                     'full_text': '%s' % temp
+                 })
+
+        # CPU
         update_cpu(j[0])
 
-        #LOAD
+        # LOAD
         update_load(j[2])
 
-        #WIRELESS
+        # WIRELESS
         wireless = get_wireless_json_part(j)
         minimize_wireless_msg(wireless)
-        
+
         # and echo back new encoded json
         print_line(prefix+json.dumps(j))
